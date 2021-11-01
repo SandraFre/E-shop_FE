f69e8e42b87e4720b4495339183158bd2926aa06
SandraFre <sandra.email@gmail.com>
2021-11-01 22:23:42 +0200

    popular products added and styled

--- js/main.js
+++ js/main.js
@@ -11,34 +11,41 @@ $(function () {
 
     function successHandler(response) {
         products = response;
+        fillPopular(products)
     }
 
     function errorHandler(response) {
         console.log(response.status);
     }
 
-    fillPopular(products);
+    console.log('ready');
+
+    console.log(products);
+
+
+
     
-    
+       
 
     function fillPopular(array) {
         let cards = '';
         array.forEach(function (item) {
-            // if (item.rate > 4) {
+            if (item.rating.rate > 4) {
                 cards +=
                     '<div class="col-12 col-sm-6 col-md-4 col-lg-3 d-flex justify-content-md-between justify-content-center">' +
-                    '<div class="card mt-4" 4 style="width: 18rem;">'
-                '<img src="' + item.image + '" class="card-img-top">' +
-                    '<div class="card-body">' +
+                    '<div class="card mt-4">'+
+                '<img src="' + item.image + '" class="card-img-top card-image">' +
+                    '<div class="card-body d-flex flex-column justify-content-between">' +
                     '<h5 class="card-title">' + item.title + '</h5>' +
-                        '<p class="card-text text-ellipsis">'+item.description + '</p>' +
+                    '<p class="card-text text-ellipsis">' + item.description + '</p>' +
+                    '<h6>Price: ' + item.price + ' Eur</h6>' +
                         '<a href="#" class="btn btn-warning ">Add to cart</a>'+
                     '</div >'+
                 '</div >'+
             '</div >'
 
                     $('#popularProducts').html(cards);
-            // }
+            }
         })
     }
 
