97e090f5193e627d62a786a81baadfba70cf8103
SandraFre <sandra.email@gmail.com>
2021-11-02 13:20:12 +0200

    products page filled by js data

--- js/main.js
+++ js/main.js
@@ -11,13 +11,13 @@ $(function () {
 
     function successHandler(response) {
         products = response;
-        fillPopular(products)
+        fillPopular(products);
+        allProducts(products);
     }
 
     function errorHandler(response) {
         console.log(response.status);
     }
-
        
     function fillPopular(array) {
         let cards = '';
@@ -38,6 +38,27 @@ $(function () {
 
                     $('#popularProducts').html(cards);
             }
+        })
+    }
+
+    function allProducts(array) {
+        let cards = '';
+        array.forEach(function (item) {
+                cards +=
+                    '<div class="col-12 col-sm-6 col-md-4 col-lg-3 d-flex justify-content-md-between justify-content-center">' +
+                    '<div class="card mt-4">'+
+                '<img src="' + item.image + '" class="card-img-top card-image">' +
+                    '<div class="card-body d-flex flex-column justify-content-between">' +
+                    '<h5 class="card-title">' + item.title + '</h5>' +
+                    '<p class="card-text text-ellipsis">' + item.description + '</p>' +
+                    '<h6>Price: ' + item.price + ' Eur</h6>' +
+                        '<a href="#" class="btn btn-warning ">Add to cart</a>'+
+                    '</div >'+
+                '</div >'+
+            '</div >'
+
+                    $('#allProducts').html(cards);
+
         })
     }
 
