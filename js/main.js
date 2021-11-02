d1417b78ee78869825702f1dcd2d1e34a6938adc
SandraFre <sandra.email@gmail.com>
2021-11-02 18:48:20 +0200

    replaced functions

--- js/main.js
+++ js/main.js
@@ -50,6 +50,28 @@ $(function () {
     }
 
 
+    function fillPopular(array) {
+        let cards = '';
+        array.forEach(function (item) {
+            if (item.rating.rate > 4) {
+                cards +=
+                    '<div class="col-12 col-sm-6 col-md-4 col-lg-3 d-flex justify-content-md-between justify-content-center">' +
+                    '<div class="card mt-4">' +
+                    '<img src="' + item.image + '" class="card-img-top card-image">' +
+                    '<div class="card-body d-flex flex-column justify-content-between">' +
+                    '<h5 class="card-title">' + item.title + '</h5>' +
+                    '<p class="card-text text-ellipsis">' + item.description + '</p>' +
+                    '<h6>Price: ' + item.price + ' Eur</h6>' +
+                    '<a id="addToCart" href="" class="btn btn-warning ">Add to cart</a>' +
+                    '</div >' +
+                    '</div >' +
+                    '</div >'
+
+                $('#popularProducts').html(cards);
+            }
+        })
+    }
+
     function fillSearched(array) {
         $('#popular').addClass('d-none');
         $('#searchedItems').removeClass('d-none');
@@ -79,31 +101,6 @@ $(function () {
         }
     }
 
-
-
-
-    function fillPopular(array) {
-        let cards = '';
-        array.forEach(function (item) {
-            if (item.rating.rate > 4) {
-                cards +=
-                    '<div class="col-12 col-sm-6 col-md-4 col-lg-3 d-flex justify-content-md-between justify-content-center">' +
-                    '<div class="card mt-4">' +
-                    '<img src="' + item.image + '" class="card-img-top card-image">' +
-                    '<div class="card-body d-flex flex-column justify-content-between">' +
-                    '<h5 class="card-title">' + item.title + '</h5>' +
-                    '<p class="card-text text-ellipsis">' + item.description + '</p>' +
-                    '<h6>Price: ' + item.price + ' Eur</h6>' +
-                    '<a href="#" class="btn btn-warning ">Add to cart</a>' +
-                    '</div >' +
-                    '</div >' +
-                    '</div >'
-
-                $('#popularProducts').html(cards);
-            }
-        })
-    }
-
     function allProducts(array) {
         let cards = '';
         array.forEach(function (item) {
@@ -115,7 +112,7 @@ $(function () {
                 '<h5 class="card-title">' + item.title + '</h5>' +
                 '<p class="card-text text-ellipsis">' + item.description + '</p>' +
                 '<h6>Price: ' + item.price + ' Eur</h6>' +
-                '<a href="#" class="btn btn-warning ">Add to cart</a>' +
+                '<a id="addToCart" href="" class="btn btn-warning ">Add to cart</a>' +
                 '</div >' +
                 '</div >' +
                 '</div >'
